ce19c9002f88f719c0a9f8a901b9dc6f5cadacb1
Alive24 <xct24@live.com>
2024-12-23 23:17:35 +0000

    fix: allow switching to blake2b-ref for WASM

--- src/blake2b.rs
+++ src/blake2b.rs
@@ -1,5 +1,12 @@
 use crate::{traits::Hasher, H256};
-use blake2b_rs::{Blake2b, Blake2bBuilder};
+#[cfg(feature = "ckb-contract")]
+pub use blake2b_ref::{Blake2b, Blake2bBuilder};
+
+#[cfg(all(not(feature = "ckb-contract"), target_family = "wasm"))]
+pub use blake2b_ref::{Blake2b, Blake2bBuilder};
+
+#[cfg(all(not(feature = "ckb-contract"), not(target_family = "wasm")))]
+pub use blake2b_rs::{Blake2b, Blake2bBuilder};
 
 const BLAKE2B_KEY: &[u8] = &[];
 const BLAKE2B_LEN: usize = 32;
